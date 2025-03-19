15697c2cc2269198df4b3240af72c2128778d0f4
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-19 16:20:34 +0700

    Membuat sequalize model

---
+++ server/models/index.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+const dbConfig = require("../config/db.config");
+
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    port: dbConfig.PORT,
+    pool: dbConfig.pool,
+    logging: false
+  }
+);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("./user.model")(sequelize, Sequelize);
+
+module.exports = db;
