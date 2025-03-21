5b57e69e46d6690534925133ad40cd9840caab2d
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-21 14:49:58 +0700

    Mendaftarkan route biodata

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/user.routes');
+const biodataRoutes = require("./routes/biodata.routes");
 
 app.use(cors());
 app.use(express.json());
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 });
 app.use('/api', authRoutes); // login dan register
 app.use('/api', userRoutes); // profile
+app.use("/api/biodata", biodataRoutes);
 
 const PORT = process.env.PORT || 3200;
 app.listen(PORT, () => {
