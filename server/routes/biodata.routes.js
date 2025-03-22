f57edae5475c5309bd93952b7157a952e1fcdecc
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-22 21:12:18 +0700

    Merubah route dan memindahkan control setiap route nya ke biodataController

--- server/routes/biodata.routes.js
+++ server/routes/biodata.routes.js
@@ -1,127 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models");
 const upload = require("../middlewares/upload");
 const verifyToken = require("../middlewares/authMiddleware");
 
-// create biodata
-router.post("/", verifyToken, upload.single("photo"), async (req, res) => {
-  try {
-    const {name, email, phone, address} = req.body;
-    const photo = req.file ? req.file.buffer : null;
+const {
+  createBiodata,
+  getAllBiodata,
+  getBiodataById,
+  updateBiodata,
+  deleteBiodata
+} = require("../controllers/biodataController");
 
-    const newData = await db.biodata.create({
-      name,
-      email,
-      phone,
-      address,
-      photo,
-      userId: req.user.id,
-    });
-    res.status(201).json({message: "Biodata berhasil dibuat", data: newData});
-  } catch (err) {
-    res.status(500).json({message: "Gagal membuat biodata", error: err});
-  }
-});
+// route untuk menambah biodata
+router.post("/", verifyToken, upload.single("photo"), createBiodata);
 
-// read/get all semua biodata
-router.get("/", verifyToken, async (req, res) => {
-  try {
-    const whereCondition =
-      req.user.role === "admin" ? {} : {userId: req.user.id};
+// route untuk mendapatkan semua biodata
+router.get("/", verifyToken, getAllBiodata);
 
-    const data = await db.biodata.findAll({where: whereCondition});
+// route untuk melihat detail biodata by id
+router.get("/:id", verifyToken, getBiodataById);
 
-    const formattedData = data.map((item) => ({
-      id: item.id,
-      name: item.name,
-      address: item.address,
-      phone: item.phone,
-      photo: item.photo
-        ? `data:image/jpeg;base64,${item.photo.toString("base64")}`
-        : null,
-      createdAt: item.createdAt,
-      updatedAt: item.updatedAt,
-    }));
+// route untuk memperbarui data biodata
+router.put("/:id", verifyToken, upload.single("photo"), updateBiodata);
 
-    res.json(formattedData);
-  } catch (err) {
-    res.status(500).json({message: "Gagal mengambil data", error: err});
-  }
-});
+// route untuk menghapus biodata
+router.delete("/:id", verifyToken, deleteBiodata);
 
-// read detail biodata by id
-router.get("/:id", verifyToken, async (req, res) => {
-  try {
-    const item = await db.biodata.findByPk(req.params.id);
-    if (!item) return res.status(404).json({message: "Data tidak ditemukan"});
-
-    // hanya pemilik data atau admin
-    if (req.user.role !== "admin" && item.userId !== req.user.id) {
-        return res.status(403).json({ message: "Akses ditolak" });
-    }
-
-    const formattedItem = {
-      id: item.id,
-      name: item.name,
-      address: item.address,
-      phone: item.phone,
-      photo: item.photo
-        ? `data:image/jpeg;base64,${item.photo.toString("base64")}`
-        : null,
-      createdAt: item.createdAt,
-      updatedAt: item.updatedAt,
-    };
-
-    res.json(formattedItem);
-  } catch (err) {
-    res.status(500).json({message: "Gagal mengambil detail data", error: err});
-  }
-});
-
-// update biodata
-router.put("/:id", verifyToken, upload.single("photo"), async (req, res) => {
-    try {
-      const { name, email, phone, address } = req.body;
-      const photo = req.file ? req.file.buffer : null;
-  
-      const biodata = await db.biodata.findByPk(req.params.id);
-      if (!biodata) return res.status(404).json({ message: "Data tidak ditemukan" });
-  
-      // hanya pemilik data atau admin
-      if (req.user.role !== "admin" && biodata.userId !== req.user.id) {
-        return res.status(403).json({ message: "Akses ditolak" });
-      }
-  
-      biodata.name = name ?? biodata.name;
-      biodata.email = email ?? biodata.email;
-      biodata.address = address ?? biodata.address;
-      biodata.phone = phone ?? biodata.phone;
-      if (photo) biodata.photo = photo;
-  
-      await biodata.save();
-      res.status(201).json({ message: "Biodata berhasil diupdate", biodata });
-    } catch (err) {
-      res.status(500).json({ message: "Gagal update data", error: err });
-    }
-});
-  
-// delete biodata
-router.delete("/:id", verifyToken, async (req, res) => {
-    try {
-      const biodata = await db.biodata.findByPk(req.params.id);
-      if (!biodata) return res.status(404).json({ message: "Data tidak ditemukan" });
-  
-      // hanya pemilik data atau admin
-      if (req.user.role !== "admin" && biodata.userId !== req.user.id) {
-        return res.status(403).json({ message: "Akses ditolak" });
-      }
-  
-      await biodata.destroy();
-      res.json({ message: "Biodata berhasil dihapus" });
-    } catch (err) {
-      res.status(500).json({ message: "Gagal hapus data", error: err });
-    }
-});
-  
 module.exports = router;
