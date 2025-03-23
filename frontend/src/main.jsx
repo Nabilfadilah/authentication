9ac759218b6d7f830b72f02504f263cc148d0fd4
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-23 16:06:11 +0700

    Menambah authProvider untuk membungkus App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {StrictMode} from "react"; // impor StrictMode dari React untuk membantu mendeteksi potensi masalah dalam aplikasi
+import {createRoot} from "react-dom/client"; // impor fungsi createRoot dari ReactDOM untuk merender aplikasi ke dalam elemen HTML
+import "./index.css";
+import App from "./App.jsx";
+import {AuthProvider} from "./context/AuthContext.jsx"; // impor AuthProvider untuk menyediakan konteks autentikasi ke seluruh aplikasi
 
-createRoot(document.getElementById('root')).render(
+// merender aplikasi ke dalam elemen dengan id "root" di file HTML utama
+createRoot(document.getElementById("root")).render(
+  // StrictMode membantu menemukan potensi masalah dalam pengembangan
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* AuthProvider membungkus App untuk menyediakan konteks autentikasi ke seluruh aplikasi */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
