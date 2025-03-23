200c52bb69b6e3d17a4dae9f6a8ff3e85831d585
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-23 16:06:39 +0700

    Menambah broserRouter untuk fitur routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+// impor komponen ProtectedRoute untuk membatasi akses ke halaman tertentu
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
-    <div>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600 p-5 bg-gray-500">
-          Hello Tailwind!
-        </h1>
-      </div>
-    </div>
+    // BrowserRouter digunakan sebagai pembungkus utama untuk mengaktifkan fitur routing di aplikasi
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* route untuk halaman dashboard, yang hanya bisa diakses jika user sudah login */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
