ceec40f799714c07052ab0244dc53d4d2076379a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-24 14:30:36 +0700

    Menambah route user get by id

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -9,7 +9,8 @@ const {
   getAllUsers,
   updateUser,
   deleteUser,
-  resetUserPassword
+  resetUserPassword,
+  getUserById
 } = require("../controllers/userController");
 
 // rute untuk mendapatkan profil pengguna yang sedang login
@@ -19,12 +20,15 @@ router.get("/profile", verifyToken, getUserProfile);
 router.get("/users", verifyToken, isAdmin, getAllUsers);
 
 // rute untuk memperbarui data pengguna (baik oleh user sendiri atau admin)
-router.put("/users/:id", verifyToken, updateUser);
+router.put("/user/:id/edit", verifyToken, updateUser);
 
 // rute untuk menghapus pengguna (hanya admin)
-router.delete("/users/:id", verifyToken, isAdmin, deleteUser);
+router.delete("/user/:id", verifyToken, isAdmin, deleteUser);
 
 // rute untuk mereset password pengguna (hanya admin)
-router.put("/users/:id/reset-password", verifyToken, isAdmin, resetUserPassword);
+router.put("/admin/reset-password/:id", verifyToken, isAdmin, resetUserPassword);
+
+// route untuk get user by id
+router.get("/users/:id", verifyToken, isAdmin, getUserById);
 
 module.exports = router;
