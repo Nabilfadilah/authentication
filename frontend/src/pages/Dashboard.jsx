daeb94006021060dbcc71b72caa387d7c20f0ad6
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-27 14:06:43 +0700

    Menambah element untuk page dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import {useAuth} from "../context/AuthContext";
 import {Link, useNavigate} from "react-router-dom";
+import Typography from "../components/elements/text/Typography";
+import ButtonAll from "../components/elements/button/Index";
 
 const Dashboard = () => {
   const {user, logout} = useAuth();
@@ -14,39 +16,35 @@ const Dashboard = () => {
     <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg">
       {/* Header */}
       <div className="mb-6">
-        <h2 className="text-2xl font-bold text-gray-800">Dashboard</h2>
-        <p className="text-gray-600">
+        <Typography className="text-2xl font-bold text-gray-800">
+          Dashboard
+        </Typography>
+        <Typography className="text-gray-600">
           Selamat datang, <span className="font-semibold">{user?.name}</span>
-        </p>
-        <p className="text-gray-500">
+        </Typography>
+        <Typography className="text-gray-500">
           Role: <span className="font-medium">{user?.role}</span>
-        </p>
+        </Typography>
       </div>
 
       {/* Tombol Navigasi */}
       <div className="flex gap-4">
-        <Link
-          to="/biodata"
-          className="bg-blue-600 text-white text-sm px-4 py-1 rounded-lg hover:bg-blue-700 transition duration-200"
-        >
-          Biodata
-        </Link>
+        <ButtonAll className="bg-blue-700 hover:bg-blue-800">
+          <Link to="/biodata">Biodata</Link>
+        </ButtonAll>
 
         {user.role === "admin" && (
-          <Link
-            to="/users"
-            className="bg-green-600 text-white text-sm px-4 py-1 rounded-lg hover:bg-green-700 transition duration-200"
-          >
-            Users
-          </Link>
+          <ButtonAll className="bg-green-700 hover:bg-green-800">
+            <Link to="/users">Users</Link>
+          </ButtonAll>
         )}
 
-        <button
+        <ButtonAll
           onClick={handleLogout}
-          className="bg-red-600 text-white text-sm px-4 py-1 rounded-lg hover:bg-red-700 transition duration-200"
+          className="bg-red-600 hover:bg-red-700 transition duration-200"
         >
           Logout
-        </button>
+        </ButtonAll>
       </div>
     </div>
   );
