0304b999c1535683e8f1ec7d556f3633768b4e2d
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-27 13:59:39 +0700

    Membuat element input

---
+++ frontend/src/components/elements/input/Input.jsx
@@ -0,0 +1,21 @@
+import {forwardRef} from "react";
+
+const Input = forwardRef((props, ref) => {
+  const {type, placeholder, name, value, onChange} = props;
+
+  return (
+    <input
+      type={type}
+      className="text-sm border rounded w-full py-2 px-2 text-slate-700 placeholder:opacity-50"
+      placeholder={placeholder}
+      name={name}
+      id={name}
+      ref={ref}
+      value={value}
+      onChange={onChange}
+      required
+    />
+  );
+});
+
+export default Input;
