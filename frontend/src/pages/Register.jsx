fd90739219f3cc6d2205a3ea7372ac678f430b36
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-05-08 14:24:47 +0700

    Merubah input menjadi InputForm untuk register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
 import {useState} from "react";
 import axios from "../utils/axiosInstance"; // impor instance axios yang sudah dikonfigurasi untuk komunikasi dengan backend
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import Typography from "../components/elements/text/Typography";
 import ButtonAll from "../components/elements/button/Index";
+import InputForm from "../components/elements/input/Index";
 
 // komponen Register untuk menangani pendaftaran user
 const Register = () => {
@@ -39,26 +40,39 @@ const Register = () => {
         {/* form untuk registrasi */}
         <form onSubmit={handleSubmit} className="space-y-4">
           {/* input untuk Nama */}
-          <input
+          <InputForm
+            label="Nama"
             type="text"
             placeholder="Nama"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="focus:ring-2 focus:ring-blue-500"
             onChange={(e) => setForm({...form, name: e.target.value})}
           />
+
           {/* input untuk Email */}
-          <input
+          <InputForm
+            label="Email"
             type="email"
             placeholder="Email"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="focus:ring-2 focus:ring-blue-500"
             onChange={(e) => setForm({...form, email: e.target.value})}
           />
+
           {/* input untuk Password */}
-          <input
+          <InputForm
+            label="Password"
             type="password"
             placeholder="Password"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="focus:ring-2 focus:ring-blue-500"
             onChange={(e) => setForm({...form, password: e.target.value})}
           />
+
+          <div className="text-sm text-gray-500">
+            Kamu sudah memiliki akun,{" "}
+            <Link to="/login" className="text-blue-500 hover:underline">
+              <b>Login</b>
+            </Link>
+          </div>
+
           {/* tombol Submit */}
           <ButtonAll
             type="submit"
